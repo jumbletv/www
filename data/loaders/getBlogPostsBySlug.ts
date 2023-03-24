c8165c4aaff799dd24f6fcdf67b8e1d4bce50017
Laurent Fabre <laurent@fabre.me>
2023-03-24 02:59:14 +0100

    add all SSG content helpders for each collection

---
+++ data/loaders/getBlogPostsBySlug.ts
@@ -0,0 +1,43 @@
+import BlogPosts from 'data/cms/BlogPosts.json'
+import Authors from 'data/cms/Authors.json'
+import Tags from 'data/cms/Tags.json'
+import Types from 'data/cms/Types.json'
+import {BlogPost} from 'types/cms/BlogPost'
+import {Author} from "@/types/cms/Author";
+import {Tag} from "@/types/cms/Tag";
+import {Type} from "@/types/cms/Type";
+
+export function getBlogPostsBySlug(slug?: string): BlogPost[] {
+  return BlogPosts.map((blogPost) => {
+    if (blogPost.slug !== slug) return
+    const author = Authors.find((author) => author["_id"] === blogPost.author)
+    const tags = Tags.filter((tag) => blogPost.tags.includes(tag["_id"]))
+    const relatedPosts = BlogPosts.filter((related) => blogPost['related-posts'].includes(related["_id"]))
+    const type = Types.find((type) => type["_id"] === blogPost["type"])
+    const link = "/the-jumblog/" + blogPost.slug
+
+    const finalRelated: BlogPost[] = relatedPosts.map((related) => {
+      const relatedAuthor: Author = Authors.find((author) => author["_id"] === related.author)
+      const relatedTags: Tag[] = Tags.filter((tag) => related.tags.includes(tag["_id"]))
+      const relatedType: Type = Types.find((type) => type["_id"] === related["type"])
+      const link = "/the-jumblog/" + related.slug
+
+      return {
+        ...related,
+        authorRef: relatedAuthor,
+        tagsRef: relatedTags,
+        typeRef: relatedType,
+        link,
+      }
+    });
+
+    return {
+      ...blogPost,
+      authorRef: author,
+      tagsRef: tags,
+      typeRef: type,
+      relatedPostsRef: finalRelated,
+      link,
+    }
+  })
+}
