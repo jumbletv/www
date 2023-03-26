699eb046ad4a95d76b2cef953db2e2bfc21d9900
yacine.chibane <yacine.chibane@legalplace.fr>
2023-03-27 00:53:24 +0200

    fix: fixed missing blogpost type import

--- types/cms/Tag.ts
+++ types/cms/Tag.ts
@@ -1,25 +1,24 @@
-import { BlogPost } from './BlogPost';
+import type { BlogPost } from "./BlogPost";
 
 export type Tag = {
-    _archived: boolean;
-    _cid: string;
-    _draft: boolean;
-    _id: string;
-    'created-by': string;
-    'created-on': string;
-    description?: string;
-    icon?: {
-      alt: any;
-      fileId: string;
-      url: string;
-    };
-    name: string;
-    'published-by': string;
-    'published-on': string;
-    slug: string;
-    'updated-by': string;
-    'updated-on': string;
-    link?: string;
-    relatedPostsRef?: BlogPost[];
-  }
-  
+  _archived: boolean;
+  _cid: string;
+  _draft: boolean;
+  _id: string;
+  "created-by": string;
+  "created-on": string;
+  description?: string;
+  icon?: {
+    alt: any;
+    fileId: string;
+    url: string;
+  };
+  name: string;
+  "published-by": string;
+  "published-on": string;
+  slug: string;
+  "updated-by": string;
+  "updated-on": string;
+  link?: string;
+  relatedPostsRef?: BlogPost[];
+};
